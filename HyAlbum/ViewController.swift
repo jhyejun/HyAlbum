1c92a1d4366235cf72e54f2c881a6c9f9c91aa42
jhyejun <hyejun417@gmail.com>
2018-05-16 17:26:59 +0900

    cocoapod 설치 및 슬라이드쇼 샘플 코드 적용
    cocoapod 설치 및 슬라이드쇼 샘플 코드 적용

--- HyAlbum/ViewController.swift
+++ HyAlbum/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slideShow: ImageSlideshow!
+    
+    let kingfisherSource = [KingfisherSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, KingfisherSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, KingfisherSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.slideShow.backgroundColor = UIColor.white
+        self.slideShow.slideshowInterval = 1.0
+        self.slideShow.pageControlPosition = PageControlPosition.underScrollView
+        self.slideShow.pageControl.currentPageIndicatorTintColor = UIColor.lightGray
+        self.slideShow.pageControl.pageIndicatorTintColor = UIColor.black
+        self.slideShow.contentScaleMode = UIViewContentMode.scaleAspectFill
+        
+        // optional way to show activity indicator during image load (skipping the line will show no activity indicator)
+        self.slideShow.activityIndicator = DefaultActivityIndicator()
+        self.slideShow.currentPageChanged = { page in
+            print("current page:", page)
+        }
+        
+        // can be used with other sample sources as `afNetworkingSource`, `alamofireSource` or `sdWebImageSource` or `kingfisherSource`
+        self.slideShow.setImageInputs(kingfisherSource)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'HyAlbum' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HyAlbum
+  pod 'Alamofire', :inhibit_warnings => true
+  pod 'ObjectMapper', :inhibit_warnings => true
+  pod 'ImageSlideshow', '~> 1.5'
+  pod 'ImageSlideshow/Kingfisher'
+
+end
